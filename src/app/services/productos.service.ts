d0fac85c47259ba06ba42347343beab6fae2556f
Edwin Gonzalo Pulgarin <gonzalo.pulgarin@gmail.com>
2020-05-30 16:17:07 -0500

    finalizando clase 7 arreglado el input buscar

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+// import { Producto } from '../interfaces/producto.interface';
+import { resolve } from '../../../node_modules/@types/q';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,24 +12,59 @@ export class ProductosService {
 
   cargando = true;
   productos: any[] = [];
+  productosFiltrado: any[] = [];
 
-  constructor( private http: HttpClient) { 
+  constructor( private http: HttpClient) {
     this.cargarProductos();
   }
 
-  private cargarProductos(){
+  private cargarProductos() {
 
-    this.http.get('https://curso-angular-fe3e7.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: any[]) => {
-        this.productos = resp;
-        // console.log(resp);
-        setTimeout(() => {
+    return new Promise(  ( resolve, reject ) => {
+
+        this.http.get('https://curso-angular-fe3e7.firebaseio.com/productos_idx.json')
+        .subscribe((resp: any[]) => {
+          this.productos = resp;
           this.cargando = false;
-        }, 500);
+          resolve();
+      });
 
-      }) ;
-
-  } 
+    });
 
 
+  }
+
+  getProducto( id: string ){
+    return this.http.get(`https://curso-angular-fe3e7.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto( termino: string ) {
+
+    if (this.productos.length === 0){
+          this.cargarProductos().then( () => {
+          this.filtrarProductos( termino );
+          });
+
+    }else{
+      this.filtrarProductos( termino );
+    }
+
+
+  }
+
+  private filtrarProductos( termino: string ){
+      this.productosFiltrado = [];
+      termino = termino.toLocaleLowerCase();
+
+
+      this.productos.forEach( prod => {
+
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+
+        if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0  ) {
+          this.productosFiltrado.push( prod );
+        }
+
+      });
+  }
 }
